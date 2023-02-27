ae3a1ebb8717ed1c400c57c17a72e48225402526
integralIndividual <126520945+integralIndividual@users.noreply.github.com>
2023-02-27 16:16:22 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// written by Him
+// made on 2/22/2023
+
+// slightly modified on 2/24/2023
+
+const express = require("express");
+const app = express()
+
+// why isn't this enabled by default??
+app.use(express.json());
+
+const discord = require("discord.js");
+
+app.post('/', (request, response) => {
+  let webhook = new discord.WebhookClient({ url: request.body.webhook });
+  let webhookResponse = webhook.send(request.body);
+
+  response.json({ response: webhookResponse });
+});
+
+app.listen(process.env.PORT);
